b66d1ebfc792b64571a6f5e57d77a837ffa2d022
Gabriel Meira <meiragabriel456@gmail.com>
2022-09-21 00:57:33 -0300

    projeto NLW-esports completo

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,32 +1,64 @@
-import { StatusBar } from 'react-native';
-
+import { useEffect, useRef } from "react";
+import { StatusBar } from "react-native";
 import {
   useFonts,
   Inter_400Regular,
   Inter_600SemiBold,
   Inter_700Bold,
-  Inter_900Black
-} from '@expo-google-fonts/inter';
+  Inter_900Black,
+} from '@expo-google-fonts/inter'
+import { Subscription } from 'expo-modules-core';
+import * as Notifications from 'expo-notifications';
 
-import { Background } from './src/components/Background';
-import { Home } from './src/screens/Home';
-import { Loading } from './src/components/Loading';
+import { Routes } from './src/routes';
+import { Background } from "./src/components/Background";
+import { Loading } from "./src/components/Loading";
+
+import './src/service/notificationConfigs';
+import { getPushNotificationToken } from "./src/service/getPushNotificationToken";
 
 export default function App() {
+  const getNotificationListener = useRef<Subscription>();
+  const responseNotificationListener = useRef<Subscription>();
+
+  useEffect(() => {
+    getPushNotificationToken()
+  }, []);
+
+  useEffect(() => {
+    getNotificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+      console.log(notification)
+    });
+
+    responseNotificationListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log(response)
+    });
+
+    return () => {
+      if (getNotificationListener.current && responseNotificationListener.current) {
+        Notifications.removeNotificationSubscription(getNotificationListener.current);
+        Notifications.removeNotificationSubscription(responseNotificationListener.current);
+      }
+    }
+  }, []);
+
   const [fontsLoaded] = useFonts({
     Inter_400Regular,
     Inter_600SemiBold,
     Inter_700Bold,
-    Inter_900Black
-  })
+    Inter_900Black,
+  });
+
   return (
     <Background>
       <StatusBar
         barStyle="light-content"
-        backgroundColor="trançsparent"
-        translucent 
+        backgroundColor="transparent"
+        translucent
       />
-      {fontsLoaded ? <Home /> : <Loading />}
+
+      {fontsLoaded ? <Routes /> : <Loading />}
     </Background>
   );
 }
+
